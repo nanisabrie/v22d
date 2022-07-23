670b206bff22c0177dce797a100d77652d70a7b0
nanisabrie <94164611+nanisabrie@users.noreply.github.com>
2022-07-24 00:27:48 +0800

    Update group-kick.js
    Shshs

--- plugins/group-kick.js
+++ plugins/group-kick.js
@@ -1,24 +1,18 @@
-import { areJidsSameUser } from '@adiwajshing/baileys'
 let handler = async (m, { conn, participants }) => {
-    let users = m.mentionedJid.filter(u => !areJidsSameUser(u, conn.user.id))
-    let kickedUser = []
-    for (let user of users)
-        if (user.endsWith('@s.whatsapp.net') && !(participants.find(v => areJidsSameUser(v.id, user)) || { admin: true }).admin) {
-            const res = await conn.groupParticipantsUpdate(m.chat, [m.sender], "remove")
-            kickedUser.concat(res)
-            await delay(1 * 1000)
-        }
-    m.reply(`Succes kick ${kickedUser.map(v => '@' + v.split('@')[0])}`, null, { mentions: kickedUser })
-
+  if (!m.mentionedJid[0] && !m.quoted) throw 'Tag someone in the group to remove them'
+  let user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender
+  let owr = m.chat.split`-`[0]
+  if (user.includes(owr)) return m.reply('I cant remove him because he/she created the group')
+  conn.groupParticipantsUpdate(m.chat, [user], 'remove')
+  m.reply(`Success kick *@${user.split('@')[0]}*`, null, { mentions: [user] })
 }
-handler.help = ['kick', '-'].map(v => 'o' + v + ' @user')
-handler.tags = ['owner']
-handler.command = /^(gkick|g-)$/i
 
-handler.owner = true
+handler.help = ['kick @user']
+handler.tags = ['group']
+handler.command = /^(kick)$/i
+
+handler.admin = true
 handler.group = true
 handler.botAdmin = true
 
 export default handler
-
-const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
