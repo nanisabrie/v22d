1071d09f888546d07bacbca61ecd365d20163031
nanisabrie <94164611+nanisabrie@users.noreply.github.com>
2022-07-25 01:01:56 +0800

    Update weebs-hentai-neko.js
    Susue

--- plugins/weebs-hentai-neko.js
+++ plugins/weebs-hentai-neko.js
@@ -472,5 +472,6 @@ default:
 handler.help = ['nsfw <type>', 'hentai <type>']
 handler.tags = ['nsfw', 'premium']
 handler.command = /^(nsfw|hentai)/i
+handler.premium = true
 
 export default handler
